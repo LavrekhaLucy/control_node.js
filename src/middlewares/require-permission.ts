bea54fba40d5ad0d12484bdd7de39874f2eaa283
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-08-17 12:50:10 +0300

    the project is completed

--- src/middlewares/require-permission.ts
+++ src/middlewares/require-permission.ts
@@ -1,37 +1,19 @@
 import {NextFunction, Request, Response} from 'express';
 import {hasPermission} from '../services/permission.service';
+import {ApiError} from "../errors/api-error";
 
-
-// export const requirePermissionMiddleware = (permissionCode: PermissionEnum) => {
-//     return async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             const user = req.user as IUser;
-//             if (!user) return res.status(401).json({message: 'Unauthorized'});
-//
-//             console.log('USER ROLES AND PERMISSIONS:', user.roles);
-//
-//             const isSuperUser = user.roles?.some(role => role.permissions.includes(PermissionEnum.ALL));
-//             if (isSuperUser) return next();
-//
-//             const hasPermission = user.roles?.some(role => role.permissions.includes(permissionCode));
-//
-//             if (!hasPermission) return res.status(403).json({ message: `You do not have permission to ${permissionCode.replace('_', ' ')}` });
-//
-//             next();
-//         } catch (err) {
-//             next(err);
-//         }
-//     };
-// };
 
 export const requirePermissionMiddleware = (permissionCode: string) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             const user = req.user;
-            if (!user) return res.status(401).json({ message: 'Unauthorized' });
+            if (!user)
+                throw new ApiError( 'Unauthorized',401);
 
             const allowed = await hasPermission(user._id.toString(), permissionCode);
-            if (!allowed) return res.status(403).json({ message: `No permission: ${permissionCode}` });
+            if (!allowed)
+                throw new ApiError( `No permission: ${permissionCode}`,403);
+
 
             next();
         } catch (err) {
